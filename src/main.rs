22a0ee64218af754bab0a57482bbc3efce627f40
Jake McGrath <jake.k.mcgrath@gmail.com>
2023-10-23 21:20:06 -0500

    updated Cargo.toml to include serde dependency and made slight mods to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
-use cursive::views::{Dialog, TextView};
+use cursive::views::{SelectView, TextView};
+use cursive::Cursive;
+use serde::{Deserialize, Serialize};
 
 const LOGO: &str = "
- ______   ______     ______     __    __        ______   ______     ______     ______  
-/\__  _\ /\  ___\   /\  == \   /\ \"-./  \      /\  == \ /\  == \   /\  ___\   /\  == \ 
-\/_/\ \/ \ \  __\   \ \  __<   \ \ \-./\ \     \ \  _-/ \ \  __<   \ \  __\   \ \  _-/ 
-   \ \_\  \ \_____\  \ \_\ \_\  \ \_\ \ \_\     \ \_\    \ \_\ \_\  \ \_____\  \ \_\   
-    \/_/   \/_____/   \/_/ /_/   \/_/  \/_/      \/_/     \/_/ /_/   \/_____/   \/_/   
-                                                                                       
+                                                                        
+████████╗███████╗██████╗ ███╗   ███╗    ██████╗ ██████╗ ███████╗██████╗ 
+╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ██╔══██╗██╔══██╗██╔════╝██╔══██╗
+   ██║   █████╗  ██████╔╝██╔████╔██║    ██████╔╝██████╔╝█████╗  ██████╔╝
+   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║    ██╔═══╝ ██╔══██╗██╔══╝  ██╔═══╝ 
+   ██║   ███████╗██║  ██║██║ ╚═╝ ██║    ██║     ██║  ██║███████╗██║     
+   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝    ╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝     
+                                                                        
 ";
+
+/// Parses a JSON file that's a list of questions with various data into a question that can be used in the TUI.
+#[derive(Serialize, Deserialize)]
+struct Question {
+    prompt: String,
+    choices: Vec<String>,
+    answer: String,
+    subject_domain: String,
+    has_seen: bool,
+    answered_incorrectly: bool,
+}
 
 fn main() {
     let mut siv = Cursive::default();
-    
-    // The logo as ASCII art.
+
+    // File for the JSON exam chosen that consists of a list of questions in a format that is able to be parsed
+    // into the format of the `Question` struct above.
+    let mut exam_filename: String = String::new();
+
+    // The logo as ASCII art for display as the title of the main menu.
     let logo_view = TextView::new(LOGO);
+
+    // Select Exam Type, based on
 
     // View for selecting which exam you want to study for. Exams are based on JSON files that store the questions
     // for the respective exam.
-    let exam_select_view;
+    let exam_select_view = SelectView::<String>::new();
+
+    // RadioGroup for selecting which quiz type the user wants to conduct
+
+    // siv.run();
 }
